a2847e1f4e1bd5e0837ba5f572f5c7170d20b3ad
zahraNejati <nejatizahra4@gmail.com>
2023-11-13 10:47:02 +0330

    character detail

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import CharacterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
 import { allCharacters } from "../data/data";
+import CharacterDetail from "./components/CharacterDetail";
 function App() {
   return (
     <div className="app">
       <Navbar />
       <div className="main">
         <CharacterList characters={allCharacters} />
+        <CharacterDetail />
       </div>
     </div>
   );
